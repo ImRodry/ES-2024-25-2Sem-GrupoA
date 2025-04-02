1e850a89abeb8594f87f45860568a07a2b75ca4f
FranciscoBrito13 <franciscocrdebrito@gmail.com>
2025-04-02 13:42:03 +0100

    Função que lê o csv

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,17 @@
 import { readFileSync } from "node:fs"
+import { parse } from "csv-parse/sync"
 
 const file = readFileSync("data/Madeira-Moodle-1.1.csv", "utf-8")
 
-console.log(file.split("\n").length)
-
-const obj = {
-	abc: 5,
-	foo: () => {
-		console.log("foo")
-	},
+function readCSV(filepath: string) {
+	const content = readFileSync(filepath, "utf-8")
+	const records = parse(content, {
+		columns: true, 
+		skip_empty_lines: true,
+		delimiter: ";"
+	  })
+	return records
 }
-obj.foo()
-obj.abc = 1
 
-interface Data {
-	objectId: Map<string, number>
-	par_id?: number
-	par_num: number
-	shape_length: number
-	ola: {
-		adeus: string
-	}
-}
+const data = readCSV("data/Madeira-Moodle-1.1.csv")
+console.log(data[0])
