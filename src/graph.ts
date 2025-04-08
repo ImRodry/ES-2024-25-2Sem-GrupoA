7b8ab1223c9d2c916c8277f309dab554426727c4
xongalo <150344288+xongalo@users.noreply.github.com>
2025-04-08 15:29:29 +0100

    Apply suggestions from code review
    Co-authored-by: Rodrigo Leitão <38259440+ImRodry@users.noreply.github.com>

--- src/graph.ts
+++ src/graph.ts
@@ -2,8 +2,6 @@ import booleanIntersects from "@turf/boolean-intersects"
 import { polygon } from "@turf/helpers"
 import { Property } from "./importer"
 
-type NodeID = number
-type Graph = Map<NodeID, Set<NodeID>>
 
 // Converte a geometria para um polígono do Turf (Feature<Polygon>)
 function toTurfPolygon(geometry: [number, number][]) {
@@ -11,17 +9,17 @@ function toTurfPolygon(geometry: [number, number][]) {
 }
 
 // Função genérica de criação de grafo
-export function buildGraph(properties: Property[], getNodeId: (p: Property) => NodeID): Graph {
-	const graph: Graph = new Map()
+export function buildGraph(properties: Property[], nodeProp: keyof Property) {
+	const graph = new Map()
 	const polygons = properties.map(p => toTurfPolygon(p.geometry))
 
 	for (let i = 0; i < properties.length; i++) {
-		const nodeA = getNodeId(properties[i])
+		const nodeA = properties[i][nodeProp]
 		if (!graph.has(nodeA)) graph.set(nodeA, new Set())
 
 		for (let j = i + 1; j < properties.length; j++) {
 			if (booleanIntersects(polygons[i], polygons[j])) {
-				const nodeB = getNodeId(properties[j])
+				const nodeB = properties[i][nodeProp]
 				if (nodeA !== nodeB) {
 					graph.get(nodeA)!.add(nodeB)
 					if (!graph.has(nodeB)) graph.set(nodeB, new Set())
@@ -34,12 +32,3 @@ export function buildGraph(properties: Property[], getNodeId: (p: Property) => N
 	return graph
 }
 
-// Grafo de propriedades
-export function buildPropertyGraph(properties: Property[]): Graph {
-	return buildGraph(properties, p => p.objectId)
-}
-
-// Grafo de proprietários
-export function buildOwnerGraph(properties: Property[]): Graph {
-	return buildGraph(properties, p => p.owner)
-}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { readFileSync } from "node:fs"
 import { parseCSV, parseProperty, Property } from "./importer"
-import { buildPropertyGraph, buildOwnerGraph } from "./graph"
+import { buildGraph } from "./graph"
 
 try {
 	const rawProps = parseCSV(readFileSync("data/Madeira-Moodle-1.1.csv", "utf-8"))
@@ -17,13 +17,13 @@ try {
 	const slicedProperties = properties.slice(0, 100)
 	console.log(`A construir grafos com ${slicedProperties.length} propriedades...`)
 
-	const propertyGraph = buildPropertyGraph(slicedProperties)
+	const propertyGraph = buildGraph(slicedProperties, "objectId")
 	console.log("Grafo de propriedades (por objectId):")
 	for (const [node, neighbours] of propertyGraph.entries()) {
 		console.log(`${node} -> ${[...neighbours].join(", ")}`)
 	}
 
-	const ownerGraph = buildOwnerGraph(slicedProperties)
+	const ownerGraph = buildGraph(slicedProperties, "owner")
 	console.log("\nGrafo de proprietários (por owner):")
 	for (const [node, neighbours] of ownerGraph.entries()) {
 		console.log(`${node} -> ${[...neighbours].join(", ")}`)
