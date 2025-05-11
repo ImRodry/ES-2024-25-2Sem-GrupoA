0a959ac58f97e3cf68e0035b0c5cd6952954f6e0
eduardopio03 <eduardojosepio@gmail.com>
2025-05-11 22:47:56 +0100

    feat: calculo da area media com adjacencias

--- src/calculations.ts
+++ src/calculations.ts
@@ -1,4 +1,5 @@
 import { Property } from "./importer.ts"
+import { buildGraph } from "./graph.ts"
 
 export function averageArea(properties: Property[], regionType: "freguesia" | "municipio" | "ilha") {
 	return Object.fromEntries(
@@ -8,3 +9,48 @@ export function averageArea(properties: Property[], regionType: "freguesia" | "m
 		})
 	)
 }
+
+export function averageAreaWithAdjacency(properties: Property[], regionType: "freguesia" | "municipio" | "ilha") {
+	// Construir grafo de adjacência entre propriedades, usando objectId como nó
+	const adjacencyGraph = buildGraph(properties, "objectId")
+	const visited = new Set<number>()
+	const mergedProperties: Property[] = []
+  
+	// Percorrer as propriedades e agrupar fisicamente adjacentes do mesmo proprietário
+	for (const property of properties) {
+	  if (visited.has(property.objectId)) continue
+  
+	  const stack = [property.objectId]
+	  const mergedOwner = property.owner
+	  // Iniciar objeto "merge" com área e geometria vazias
+	  const merged: Property = { ...property, shapeArea: 0, geometry: [] as [number, number][] }
+  
+	  while (stack.length > 0) {
+		const currentId = stack.pop()!
+		if (visited.has(currentId)) continue
+		visited.add(currentId)
+  
+		const currentProp = properties.find(p => p.objectId === currentId)!
+		// Somar área e concatenar geometria
+		merged.shapeArea += currentProp.shapeArea
+		merged.geometry.push(...currentProp.geometry)
+  
+		// Explorar vizinhos adjacentes, mas apenas do mesmo proprietário
+		for (const neighborId of adjacencyGraph.get(currentId) || []) {
+		  const neighbor = properties.find(p => p.objectId === neighborId)!
+		  if (neighbor.owner === mergedOwner && !visited.has(neighborId)) {
+			stack.push(neighborId)
+		  }
+		}
+	  }
+  
+	  mergedProperties.push(merged)
+	}
+  
+	// Calcular média de área por região, usando as propriedades já fundidas
+	return Object.fromEntries(
+	  Object.entries(Object.groupBy(mergedProperties, prop => prop[regionType])).map(([region, props]) => {
+		return [region, props!.reduce((acc, prop) => acc + prop.shapeArea, 0) / props!.length]
+	  })
+	)
+  }
