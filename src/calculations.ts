5d46eab7af937354c3d9fe1402bd3b8a659edec5
xongalo <150344288+xongalo@users.noreply.github.com>
2025-05-16 17:33:39 +0100

    Update src/calculations.ts
    Co-authored-by: Rodrigo Leitão <38259440+ImRodry@users.noreply.github.com>

--- src/calculations.ts
+++ src/calculations.ts
@@ -8,7 +8,7 @@ import type { Property } from "./importer.ts"
  * Calculates the average area of properties grouped by a specified region type (freguesia, municipio, ilha).
  * @param properties Array of properties to calculate the average area for.
  * @param regionType The type of region to group properties by (freguesia, municipio, ilha).
- * @returns An object where keys are region names and values are the average area of properties in that region.
+ * @returns An object of region names mapped to the average area of properties in that region.
  */
 export function averageArea(properties: Property[], regionType: "freguesia" | "municipio" | "ilha") {
 	return Object.fromEntries(
